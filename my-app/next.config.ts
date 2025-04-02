c38fad21cdc00d45dfeede57268531b3a6160c34
Tushar <tusharkd17@gmail.com>
2025-04-02 21:10:57 +0530

    Initial project commit

---
+++ my-app/next.config.ts
@@ -0,0 +1,39 @@
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   images: {
+//     domains: ['s3.us-east-2.amazonaws.com'], 
+//     // ['s3-inventorymanagement.s3.us-east-2.amazonaws.com'],
+//   },
+// };
+
+// export default nextConfig;
+
+
+
+
+//2
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   /* config options here */
+// };
+
+// export default nextConfig;
+
+//3
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "s3-inventorymanagement.s3.us-east-2.amazonaws.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
+};
+
+export default nextConfig;
